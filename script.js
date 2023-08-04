81c09c6a548eef6da0bc70cf1d72ce7cb7260328
gitanshu18 <lusifer0418@gmail.com>
2023-08-04 23:23:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const searchbar = document.getElementById('searchbar');
+const rows = document.getElementsByClassName('product-row');
+const row_titles = [...document.getElementsByClassName('product-title')];
+
+function showSearchedProduct(product){
+    const prdt = product.toLowerCase();
+
+    for(let item = 0; item < row_titles.length; item++){
+        const rowItem = row_titles[item].textContent.toLowerCase().includes(prdt)
+        if(rowItem === false){
+            rows[item].style.display = 'none';
+        }
+    }
+}
+
+setInterval(()=>{
+    if (searchbar.value.length != 0){
+        showSearchedProduct(searchbar.value);
+    } else{
+        for(let item = 0; item < row_titles.length; item++){
+            rows[item].style.display = 'grid';
+        }
+    }
+}, 100)
